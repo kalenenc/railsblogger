bab96f42cc994a45908953fb0af0a64074ce68aa
Kalene <kalenenc@gmail.com>
2017-03-29 05:00:59 -0400

    tagging feature complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,12 +17,16 @@ class ArticlesController < ApplicationController
       @article = Article.new
    end
 
+   
+
    def create
       @article = Article.new(article_params)
       flash.notice = "Article #{@article.title} was created!"
       @article.save
       redirect_to article_path(@article)
    end
+
+
 
    def destroy
       @article = Article.find(params[:id])

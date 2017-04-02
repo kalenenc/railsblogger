e0643c2ace63b85fa906a0d981cd131b0b4240e4
Kalene <kalenenc@gmail.com>
2017-04-02 02:34:32 -0400

    authentication working, main functionality of app working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
    include ArticlesHelper #checkout helpers/articles_helper
-
+   before_filter :require_login, except: [:index, :show]
    def index
       @articles = Article.all
    end
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
       @article = Article.new
    end
 
-   
+
 
    def create
       @article = Article.new(article_params)
